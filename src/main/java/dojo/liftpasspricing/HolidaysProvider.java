9c65f445a85de413f7dfb4510ea94da8a92da29a
MichaelRWolf <MichaelRWolf@users.noreply.github.com>
2024-02-02 16:12:25 +0000

    deploy: 9e1c02f27294a60209f3965e58b047adf5e1831b

--- src/main/java/dojo/liftpasspricing/HolidaysProvider.java
+++
@@ -1,9 +0,0 @@
-package dojo.liftpasspricing;
-
-import java.sql.SQLException;
-import java.util.Date;
-import java.util.List;
-
-public interface HolidaysProvider {
-    List<Date> getHolidays() throws SQLException;
-}
